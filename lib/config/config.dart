11032951ace6474b0523665d219db1ff6df84bf2
FSociety07 <sawntharan07@gmail.com>
2022-02-13 18:58:58 +0530

    test

---
+++ lib/config/config.dart
@@ -0,0 +1,17 @@
+class configurations {
+  static const _apiKey ="AIzaSyAgkoELWMr5HLuMYIF1RmRzyWqKH3DATPU";
+  static const _authDomain = "pickngo-76ac4.firebaseapp.com";
+  static const _projectId = "pickngo-76ac4";
+  static const _storageBucket = "pickngo-76ac4.appspot.com";
+  static const _messagingSenderId = "576706625341";
+  static const _appId = "1:576706625341:web:764174d12e735c3ef04f54";
+  static const _measurementId = "G-T3MGY04HRR";
+
+// getter functions
+  String get apiKey => _apiKey;
+  String get authDomain => _authDomain;
+  String get projectId => _projectId;
+  String get storageBucket => _storageBucket;
+  String get messagingSenderId => _messagingSenderId;
+  String get appId => _appId;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pickngo/model/main_screen.dart';
+import 'package:pickngo/config/config.dart';
 
 Future<void> main() async {
+  configurations cfg = configurations();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+      apiKey: "AIzaSyAgkoELWMr5HLuMYIF1RmRzyWqKH3DATPU",
+      appId: "1:576706625341:web:764174d12e735c3ef04f54",
+      messagingSenderId: "576706625341",
+      projectId: "pickngo-76ac4",
+      authDomain:"pickngo-76ac4.firebaseapp.com",
+      storageBucket: "pickngo-76ac4.appspot.com",
+      measurementId: "G-T3MGY04HRR"));
 
   runApp(const MyApp());
 }
@@ -14,6 +24,13 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+  void initState() {
+
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+
+    });
+  }
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
